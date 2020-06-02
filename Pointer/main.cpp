0ea70469bc6ad5fa17dbea3a9492eb450648807f
Tanapol <korn_tanapol@outlook.co.th>
2020-06-02 15:10:40 +0700

    Pointer to array

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int a=5, b=10;
-    int *pointer1=&a;
-    int *pointer2=&b;
-    printf("pointer1 point to address %p has value of %d\n",pointer1,*pointer1);
-    printf("pointer2 point to address %p has value of %d\n",pointer2,*pointer2);
-    printf("----------------------------\n");
-    printf("Assign pointer2=pointer1;\n");
-    pointer2 = pointer1; // make pointer2 reference to the same address as pointer1
-    printf("pointer1 point to address %p has value of %d\n",pointer1,*pointer1);
-    printf("pointer2 point to address %p has value of %d\n",pointer2,*pointer2);
+    int a[]={12,25,30,42,53};
+    int *aPointer = a;//point to array, Note that you don't need '&' in front of the array
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",*(aPointer+i)); //looping through array using pointer
+    }
     return 0;
 }
