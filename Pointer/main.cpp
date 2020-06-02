3fb50d2eda4727048c19098d47a3c9ca7bd1a17f
Tanapol <korn_tanapol@outlook.co.th>
2020-06-02 14:54:54 +0700

    One-line pointer referencing

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -2,9 +2,8 @@
 
 int main() {
     int number=5;
-    int *numberPointer; //Declaring pointer for int data type
-    numberPointer = &number; //"Point"(Make reference) to 'number' int variable
-    printf("numberPointer is pointing to address %p\n",numberPointer); //Getting 'number' address
-    printf("number has value equal to %d",*numberPointer); //Getting 'number' value via 'numberPointer'
+    int *numberPointer=&number; //One-line referencing
+    printf("numberPointer is pointing to address %p\n",numberPointer);
+    printf("number has value equal to %d",*numberPointer);
     return 0;
 }
