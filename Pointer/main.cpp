9553783a4a8e0427e9c5f6fdcf38e9debff87015
Tanapol <korn_tanapol@outlook.co.th>
2020-06-02 15:03:18 +0700

    Pointers to same variable

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a=5,b=10;
-    int *aPointer=&a,*bPointer=&b; //making references to int variables
-    printf("Use pointer to sum to value, a=%d, b=%d, a+b=%d",*aPointer,*bPointer,*aPointer+*bPointer);
+    int a=5;
+    int *pointer1=&a, *pointer2=&a; //referencing same variable (a)
+    printf("pointer1 point to address %p has value of %d\n",pointer1,*pointer1);
+    printf("pointer2 point to address %p has value of %d\n",pointer2,*pointer2);
+    printf("----------------------------\n");
+    printf("Changing 'a' value with pointer1 to 999\n");
+    *pointer1=999;
+    printf("pointer1 point to address %p has value of %d\n",pointer1,*pointer1);
+    printf("pointer2 point to address %p has value of %d\n",pointer2,*pointer2); //Note that *pointer2 value also change
     return 0;
 }
