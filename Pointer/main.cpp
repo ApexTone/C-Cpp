16e3bb83bc30c3407a89b4500214cccd17c4e703
Tanapol <korn_tanapol@outlook.co.th>
2020-06-02 15:06:41 +0700

    Reassigning pointer reference

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int a=5;
-    int *pointer1=&a, *pointer2=&a; //referencing same variable (a)
+    int a=5, b=10;
+    int *pointer1=&a;
+    int *pointer2=&b;
     printf("pointer1 point to address %p has value of %d\n",pointer1,*pointer1);
     printf("pointer2 point to address %p has value of %d\n",pointer2,*pointer2);
     printf("----------------------------\n");
-    printf("Changing 'a' value with pointer1 to 999\n");
-    *pointer1=999;
+    printf("Assign pointer2=pointer1;\n");
+    pointer2 = pointer1; // make pointer2 reference to the same address as pointer1
     printf("pointer1 point to address %p has value of %d\n",pointer1,*pointer1);
-    printf("pointer2 point to address %p has value of %d\n",pointer2,*pointer2); //Note that *pointer2 value also change
+    printf("pointer2 point to address %p has value of %d\n",pointer2,*pointer2);
     return 0;
 }
